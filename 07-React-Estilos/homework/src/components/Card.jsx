7d6b66336e4d8e3936a9449d31d0e0a871caf4f3
Ezequiel Sánchez <esanchez@juleriaque.com.ar>
2022-04-23 12:23:21 -0300

    Homework React-Estilos

--- 07-React-Estilos/homework/src/components/Card.jsx
+++ 07-React-Estilos/homework/src/components/Card.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import styles from './Card.module.css';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
-};
+  return    <div className={ styles.cardDiv }>
+            <button  className={ styles.button } onClick={props.onClose}>X</button>
+            <h3  className={ styles.h3 }>Name: {props.name}</h3>
+                <div className={ styles.containerTemp }>
+                  <div>
+                    <p>Min</p>
+                    <p>{Math.floor(props.min/10)}</p>
+                  </div>
+                  <div>
+                    <p>Max</p>
+                    <p>{Math.floor(props.max/10)}</p>
+                  </div>  
+                  <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`}></img>
+                  </div>
+              </div>
+  };
--- 07-React-Estilos/homework/src/components/SearchBar.jsx
+++ 07-React-Estilos/homework/src/components/SearchBar.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import styles from './SearchBar.module.css';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return (
+    <div className={ styles.div }>
+      <input type="text" className={ styles.text }/>
+      <button className={ styles.button } onClick={() => props.onSearch('rosario')}>Agregar</button>
+    </div>)
 };
